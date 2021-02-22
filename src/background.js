36168ca3edb392da40ddd23380e77d1941e1f16f
GHOST <ghostdevbusiness@gmail.com>
2021-02-22 02:29:29 +0000

    feat: toggles between github and github1s

--- src/background.js
+++ src/background.js
@@ -1,19 +1,26 @@
 chrome.browserAction.onClicked.addListener(async function (tab) {
-    if (tab.url.match(/^(http(s)?):\/\/github1s.com/gm))
-        return chrome.browserAction.setPopup({
-            popup: `src/views/is-github1s.html`,
-            tabId: tab.id,
-        });
+    const isValidURL = tab.url.match(
+        /^(http(s)?):\/\/(?:github|github1s).com/gm,
+    );
 
-    if (!tab.url.match(/^(http(s)?):\/\/github.com/gm))
+    if (!isValidURL)
         return chrome.browserAction.setPopup({
             popup: `src/views/not-github.html`,
             tabId: tab.id,
         });
 
-    const basepath = tab.url.replace(/^(http(s)?):\/\/github.com/gm, '');
+    const basepath = tab.url.replace(
+        /^(http(s)?):\/\/(?:github|github1s).com/gm,
+        '',
+    );
 
-    chrome.tabs.update(tab.id, {
-        url: 'https://github1s.com' + basepath,
-    });
+    if (tab.url.match(/^(http(s)?):\/\/github1s.com/gm)) {
+        chrome.tabs.update(tab.id, {
+            url: 'https://github.com' + basepath,
+        });
+    } else {
+        chrome.tabs.update(tab.id, {
+            url: 'https://github1s.com' + basepath,
+        });
+    }
 });
